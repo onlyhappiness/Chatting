355f7a58bcf71e047818e58a0e4f1f4972c10187
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-08-11 17:23:05 +0900

    Add annotation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// express
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -16,25 +17,26 @@ connection.connect((err) => {
   console.log('Mysql Connected!!');
 
   // mysql 데이터 베이스 생성 및 연결 (접속)
-  connection.query('CREATE DATABASE express_db', (err, result) => {
-    console.log('database created!!');
-  });
+  // connection.query('CREATE DATABASE express_db', (err, result) => {
+  //   console.log('database created!!');
+  // });
 
   // 테이블 생성
-  const sql = 'create table users(id int not null primary key auto_increment, name varchar(255) not null, email varchar(255) not null, email varchar(255) not null';
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('tavle created!!')
-  });
+  // const sql = 'create table users(id int not null primary key auto_increment, name varchar(255) not null, email varchar(255) not null, email varchar(255) not null';
+  // connection.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log('table created!!')
+  // });
 });
 
 
-// 
+// 처음 페이지는 로그인 페이지로 이동
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname + '/client/views/login/index.html'))
 });
 // express에서 css 적용
 app.use('/css', express.static(__dirname+'/client/css/'))
 
+// port
 const port = 5500;
 app.listen(port, () => console.log(`listening on ${port}!!`));
