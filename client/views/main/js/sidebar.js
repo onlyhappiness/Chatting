de0001b48a7ed3233f0c62023d9db0b74303c7aa
onlyhappiness96 <onlyhappiness96@gmail.com>
2021-08-30 21:45:22 +0900

    Update react

--- client/views/main/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById('container');
-const bars = document.getElementById('bars');
-const sidebar = document.getElementById('sidebar')
-
-bars.onclick = () => {
-  container.classList.toggle('active');
-  sidebar.classList.toggle('active');
-}
